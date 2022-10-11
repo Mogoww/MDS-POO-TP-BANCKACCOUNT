c62d1e75ed0f51e171648154a160d62b70d5b058
Mogow <gauthier.mauche@gmail.com>
2022-10-11 12:28:45 +0200

    first commit

---
+++ src/BankRobber.java
@@ -0,0 +1,6 @@
+public class BankRobber {
+    public static void main(String[] args) {
+        BankAccount momsSavings = new BankAccount(1000);
+        momsSavings.balance= -100; //ERROR
+    }
+}
